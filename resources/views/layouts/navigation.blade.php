ab49b2ff285c02b4bdb5dd1a30a8f194fbe77edb
nmfuadi <nmfuadi@gmail.com>
2026-04-27 21:47:57 +0700

    hapus dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,9 +14,7 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    
 
                     @if(Auth::user()->role === 'admin')
                         <x-nav-link :href="route('admin.members')" :active="request()->routeIs('admin.members')">
