947e1aa59c4f719114789b4baf9f4f79cac0d170
nmfuadi <nmfuadi@gmail.com>
2026-04-27 23:09:59 +0700

    udah lagh

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -19,7 +19,7 @@
             <div>
                 <a href="/">
                     
-                <x-application-logo class="w-64 h-auto object-contain" />
+                <x-application-logo class="w-16 h-auto object-contain" />
                 </a>
             </div>
 
