3a52755389b0da733fcf5a9ed2374d8386ff163a
nmfuadi <nmfuadi@gmail.com>
2026-09-20 14:40:09 +0700

    dfdfd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('logo.ico') }}">
         <script src="https://cdn.tailwindcss.com"></script>
         
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
